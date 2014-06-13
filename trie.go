2d5c568b2e28dd3b7464de48f66a51c13533149a
Demitrious Kelly <apokalyptik@apokalyptik.com>
2014-06-13 12:51:56 -0700

    I believe that this makes quicktrie a radix trie

--- trie.go
+++ trie.go
@@ -1,121 +1,305 @@
 package trie
 
-import "sync"
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+var Debug bool
 
 type Trie struct {
-	source     *Trie
-	leaf       bool
-	entries    int
-	childCount int
-	children   map[uint8]*Trie
-	lock       sync.RWMutex
+	key      []byte
+	children []*Trie
+	endpoint *struct{}
 }
 
 func NewTrie() *Trie {
 	return &Trie{
-		source:   nil,
-		children: map[uint8]*Trie{},
+		children: []*Trie{},
 	}
 }
 
-func (t *Trie) Add(key string) bool {
-	if t.source == nil {
-		t.add(key, t)
+func (t *Trie) longestCommonPrefix(a, b []byte) int {
+	var i = 0
+	for ; i < len(b) && i < len(a) && b[i] == a[i]; i++ {
 	}
-	t.lock.Lock()
-	defer t.lock.Unlock()
-	return t.source == nil
+	return i
 }
 
-func (t *Trie) add(key string, source *Trie) {
-	if len(key) == 0 {
-		source.entries++
-		t.leaf = true
-		return
-	} else {
-		if v, ok := t.children[key[0]]; ok {
-			v.add(key[1:], source)
-		} else {
-			v := &Trie{
-				source:   source,
-				children: map[uint8]*Trie{},
+func (t *Trie) AddString(key string) {
+	if t.key == nil {
+		t.Add([]byte(key))
+	}
+}
+
+func (t *Trie) Add(key []byte) {
+	if t.key == nil {
+		if Debug {
+			log.Printf("add: %s\n", string(key))
+		}
+		t.add([]byte(key))
+	}
+}
+
+func (t *Trie) add(key []byte) {
+	for k, v := range t.children {
+		if lcp := t.longestCommonPrefix(t.children[k].key, key); lcp > 0 {
+			if lcp == len(key) && lcp == len(v.key) {
+				// This key exists exactly
+				// eg: have "aa", adding "aa"
+				if v.endpoint == nil {
+					v.endpoint = &struct{}{}
+				}
+			} else if lcp == len(key) {
+				// the entire key is a sub-key of the child key
+				// eg: have "aa", adding "aaa"
+				oldChild := v
+				oldChild.key = v.key[lcp:]
+				newChild := &Trie{
+					endpoint: &struct{}{},
+					key:      key[:lcp],
+					children: []*Trie{oldChild},
+				}
+				t.children[k] = newChild
+			} else if lcp == len(v.key) {
+				// the entire child key is a prefix for the key
+				// eg: have "aa", adding "aaa"
+				v.add(key[lcp:])
+			} else {
+				// the key and child key share a common prefix but are both going to
+				// end up as their own children of the common prefix on account of
+				// being larger than said prefix
+				// eg: have "abc", adding "ayz"
+				oldChild := v
+				oldChild.key = oldChild.key[lcp:]
+				newChild := &Trie{
+					key: key[:lcp],
+					children: []*Trie{
+						oldChild,
+						&Trie{
+							endpoint: &struct{}{},
+							key:      key[lcp:],
+						},
+					},
+				}
+				t.children[k] = newChild
 			}
-			t.children[key[0]] = v
-			t.childCount++
-			v.add(key[1:], source)
+			return
 		}
+	}
+	t.children = append(t.children, &Trie{key: key, endpoint: &struct{}{}})
+}
+
+func (t *Trie) DropString(key string) {
+	t.Drop([]byte(key))
+}
+
+func (t *Trie) Drop(key []byte) {
+	if t.key == nil {
+		t.drop(key)
 	}
 }
 
-func (t *Trie) Del(key string) {
-	if t.source == nil {
-		t.del(key, t)
+func (t *Trie) drop(key []byte) {
+	if t.key == nil && key == nil {
+		t.children = []*Trie{}
+		return
 	}
-	t.lock.Lock()
-	defer t.lock.Unlock()
-}
-
-func (t *Trie) del(key string, source *Trie) int {
-	if len(key) == 0 {
-		if t.leaf {
-			source.entries--
-			t.leaf = false
-		}
-	} else {
-		if v, ok := t.children[key[0]]; ok {
-			v.del(key[1:], source)
-			if v.childCount == 0 && t.leaf == false {
-				delete(t.children, key[0])
-				t.childCount--
+	for k, v := range t.children {
+		if lcp := t.longestCommonPrefix(key, v.key); lcp > 0 {
+			if lcp == len(key) {
+				if k == 0 {
+					t.children = append(t.children[1:])
+				} else {
+					t.children = append(t.children[:k], t.children[k+1:]...)
+				}
+			} else if lcp < len(v.key) {
+				// The delete key is part of, but less than the entire child key, Cannot be an exact match
+				return
+			} else {
+				// The child key is less than the delete key but it is a prefix of the delete key, recurse
+				v.drop(key[lcp:])
+				if v.endpoint == nil && len(v.children) == 0 {
+					if k == 0 {
+						t.children = append(t.children[1:])
+					} else {
+						t.children = append(t.children[:k], t.children[k+1:]...)
+					}
+				}
 			}
 		}
 	}
-	return t.childCount
 }
 
-func (t *Trie) Get(key string) bool {
-	if t.source != nil {
-		return false
-	}
-	t.lock.RLock()
-	defer t.lock.RUnlock()
-	return t.get(key)
+func (t *Trie) DelString(key string) {
+	t.Del([]byte(key))
 }
 
-func (t *Trie) get(key string) bool {
-	if len(key) == 0 {
-		return t.leaf
-	}
-	if t.childCount == 0 {
-		return false
-	}
-	if v, ok := t.children[key[0]]; ok {
-		return v.get(key[1:])
-	} else {
-		return false
+func (t *Trie) Del(key []byte) {
+	if t.key == nil {
+		if Debug {
+			log.Printf("del: %s\n", string(key))
+		}
+		t.del(key)
 	}
 }
 
-func (t *Trie) Iterate(callback func(string)) {
-	if t.source != nil {
-		return
-	}
-	t.lock.RLock()
-	defer t.lock.RUnlock()
-	if t.childCount > 0 {
-		for k, v := range t.children {
-			v.iterate(string(k), callback)
+func (t *Trie) del(key []byte) {
+	for k, v := range t.children {
+		if lcp := t.longestCommonPrefix(key, v.key); lcp > 0 {
+			if lcp < len(v.key) {
+				// Delete key is a prefix of child, but is not child or further, nothing to do
+				return
+			}
+			if lcp == len(key) && lcp == len(v.key) {
+				// This is the key we came for
+				v.endpoint = nil
+				if len(v.children) == 0 {
+					if k == 0 {
+						t.children = append(t.children[1:])
+					} else {
+						t.children = append(t.children[:k], t.children[k+1:]...)
+					}
+				}
+				return
+			}
+			if lcp == len(v.key) {
+				v.del(key[lcp:])
+				if v.endpoint == nil && len(v.children) == 0 {
+					if k == 0 {
+						t.children = append(t.children[1:])
+					} else {
+						t.children = append(t.children[:k], t.children[k+1:]...)
+					}
+				}
+				return
+			}
 		}
 	}
+	// No such key found in the tree
 }
 
-func (t *Trie) iterate(key string, callback func(string)) {
-	if t.leaf {
+func (t *Trie) GetString(key string) bool {
+	return t.Get([]byte(key))
+}
+
+func (t *Trie) Get(key []byte) bool {
+	if t.key == nil {
+		if Debug {
+			log.Printf("get: %s\n", string(key))
+		}
+		rc := make(chan bool)
+		dc := make(chan struct{})
+		gr := 0
+		for _, v := range t.children {
+			gr++
+			go func(v *Trie) {
+				if key[0] != v.key[0] {
+					rc <- false
+					return
+				}
+				lcp := t.longestCommonPrefix(key, v.key)
+				var result bool
+				if lcp == len(key) && lcp == len(v.key) {
+					result = true
+				} else {
+					result = v.get(key[lcp:])
+				}
+				select {
+				case <-dc:
+					return
+				default:
+					rc <- result
+				}
+			}(v)
+		}
+		for i := 0; i < gr; i++ {
+			if <-rc {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (t *Trie) get(key []byte) bool {
+	for _, v := range t.children {
+		if key[0] != v.key[0] {
+			continue
+		}
+		if len(key) < len(v.key) {
+			continue
+		}
+		// The following can only be true if the preceeding was true
+		if lcp := t.longestCommonPrefix(key, v.key); lcp > 0 {
+			if lcp < len(v.key) {
+				// if the common prefix less than the entirety of the child
+				// key, it cannot possibly match the child key
+				return false
+			}
+			if lcp == len(key) {
+				// the child is exactly the key we're looking for
+				if v.endpoint != nil {
+					return true
+				} else {
+					return false
+				}
+			}
+			return v.get(key[lcp:])
+		}
+	}
+	return false
+}
+
+func (t *Trie) IterateString(callback func(string)) {
+	if t.key == nil {
+		t.Iterate(func(b []byte) { callback(string(b)) })
+	}
+}
+
+func (t *Trie) Iterate(callback func([]byte)) {
+	if t.key == nil {
+		t.iterate([]byte{}, callback)
+	}
+}
+
+func (t *Trie) iterate(key []byte, callback func([]byte)) {
+	if t.endpoint != nil {
 		callback(key)
 	}
-	if t.childCount > 0 {
-		for k, v := range t.children {
-			v.iterate(key+string(k), callback)
+	for _, v := range t.children {
+		v.iterate(append(key, v.key...), callback)
+	}
+}
+
+func (t *Trie) Print() {
+	for _, v := range t.children {
+		fmt.Println(string(v.key))
+		t.iterate([]byte{}, func(k []byte) { fmt.Println(string(k)) })
+	}
+}
+
+func (t *Trie) Log(indent ...int) {
+	var indentLevel = len(indent)
+	if indentLevel > 0 {
+		indentLevel = indent[0]
+	}
+	var prefix = strings.Repeat(" ", indentLevel) + "↪-"
+	if indentLevel > 0 {
+		prefix = "|" + prefix
+	}
+	if t.key != nil {
+		if t.endpoint == nil {
+			log.Printf("  %s%s\n", prefix, string(t.key))
+		} else {
+			log.Printf("* %s%s\n", prefix, string(t.key))
 		}
+		indentLevel++
+	} else {
+		log.Printf("  ++\n")
+	}
+	for _, v := range t.children {
+		v.Log(indentLevel)
 	}
 }
--- trie_test.go
+++ trie_test.go
@@ -1,100 +1,131 @@
 package trie
 
-import (
-	"strconv"
-	"testing"
-)
+import "testing"
 
 func TestTrie(t *testing.T) {
+	Debug = true
 	trie := NewTrie()
 	if trie == nil {
 		t.Error("Expected a new Trie")
 		return
 	}
-	if trie.Get("asdf") {
+	if trie.GetString("asdf") {
 		t.Errorf("Expected get on empty trie to fail")
 	}
-	trie.Add("asdf")
-	if trie.Get("asdf") == false {
+	trie.AddString("asdf")
+	if trie.GetString("asdf") == false {
 		t.Errorf("Expected get on added key to be true")
 	}
-	if trie.Get("AAA") {
+	if trie.GetString("AAA") {
 		t.Errorf("Expected trie to be case insensitive")
 	}
-	if trie.Get("aaaa") {
+	if trie.GetString("aaaa") {
 		t.Errorf("Expected get on non-added key to still be false")
 	}
-	if trie.Get("aa") {
+	if trie.GetString("aa") {
 		t.Errorf("Expected get on non-added prefix of a key to be false")
 	}
-	trie.Add("aaab")
-	if trie.Get("aaab") == false {
+	trie.AddString("aaab")
+	if trie.GetString("aaab") == false {
 		t.Errorf("Expected get on added key to be true")
 	}
-	if trie.Get("asdf") == false {
+	if trie.GetString("asdf") == false {
 		t.Errorf("Expected get on added key to be true")
 	}
-	trie.Del("aaab")
-	if trie.Get("aaab") {
+	trie.DelString("aaab")
+	if trie.GetString("aaab") {
 		t.Errorf("Expected get on deleted key to be false")
 	}
-	if trie.Get("asdf") == false {
+	if trie.GetString("asdf") == false {
 		t.Errorf("Expected get on added key to be true")
 	}
 
-	trie.Add("bbb")
-	trie.Add("xx")
-	trie.Add("qqq")
-	trie.Add("qqqq")
-	trie.Add("qqqqq")
-	trie.Add("xxx")
-	trie.Add("xxx")
-	trie.Add("xxx")
-	trie.Del("qqq")
+	trie.AddString("y")
+	trie.AddString("yy")
+	trie.AddString("yyy")
+	trie.AddString("yyyy")
+	trie.AddString("yyyyy")
+	trie.DelString("y")
+	trie.DelString("yy")
+	trie.DelString("yyy")
+	trie.DelString("yyyy")
+	trie.DelString("yyyyy")
+
+	trie.AddString("w")
+	trie.AddString("ww")
+	trie.AddString("www")
+	trie.AddString("wwww")
+	trie.AddString("wwwww")
+	trie.DelString("wwwww")
+	trie.DelString("wwww")
+	trie.DelString("www")
+	trie.DelString("ww")
+	trie.DelString("w")
+
+	trie.AddString("z")
+	trie.AddString("zz")
+	trie.AddString("zzz")
+	trie.AddString("zzzz")
+	trie.AddString("zzzzz")
+	trie.DelString("zz")
+	trie.DelString("z")
+	trie.DelString("zzzz")
+	trie.DelString("zzz")
+	trie.DelString("zzzzz")
+
+	trie.AddString("bbb")
+	trie.AddString("xx")
+	trie.AddString("qqq")
+	trie.AddString("qqqq")
+	trie.AddString("qqqqq")
+	trie.AddString("xxx")
+	trie.AddString("xxx")
+	trie.AddString("xxx")
+	trie.DelString("qqq")
 
 	var found = 0
-	trie.Iterate(func(key string) {
-		if key == "" {
-			return
-		}
-		found++
-	})
+	trie.IterateString(func(key string) { found++ })
 	if found != 6 {
-		t.Error("Expected 6 values when iterating")
+		t.Errorf("Expected 6 values when iterating, got %d", found)
 	}
-}
 
-func BenchmarkDuplicateSmallEntries(b *testing.B) {
-	trie := NewTrie()
-	for n := 0; n < b.N; n++ {
-		trie.Add("aaa")
+	trie.AddString("nnn")
+	trie.AddString("nnnnn")
+	trie.AddString("nnnnnn")
+	trie.AddString("nnnnnnn")
+	trie.AddString("nnnnnnnn")
+	trie.AddString("nnnnnnnnn")
+
+	found = 0
+	trie.IterateString(func(key string) { found++ })
+	if found != 12 {
+		t.Errorf("Expected 12 values when iterating, got %d", found)
 	}
-}
 
-func BenchmarkDuplicateLargeEntries(b *testing.B) {
-	trie := NewTrie()
-	for n := 0; n < b.N; n++ {
-		trie.Add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+	trie.DropString("nnnnnnn")
+
+	found = 0
+	trie.IterateString(func(key string) { found++ })
+	if found != 9 {
+		t.Errorf("Expected 9 values when iterating, got %d", found)
 	}
-}
 
-func BenchmarkNonDupSmallEntries(b *testing.B) {
-	trie := NewTrie()
-	for n := 0; n < b.N; n++ {
-		trie.Add("aaa" + strconv.Itoa(n))
+	trie.DropString("nnnn")
+
+	found = 0
+	trie.IterateString(func(key string) { found++ })
+	if found != 7 {
+		t.Errorf("Expected 7 values when iterating, got %d", found)
 	}
-}
 
-func BenchmarkNonDupLargeEntries(b *testing.B) {
-	trie := NewTrie()
-	for n := 0; n < b.N; n++ {
-		trie.Add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" + strconv.Itoa(n))
+	trie.DropString("n")
+
+	found = 0
+	trie.IterateString(func(key string) { found++ })
+	if found != 6 {
+		t.Errorf("Expected 6 values when iterating, got %d", found)
 	}
-}
-
-func BenchmarkDeleteEmptyTrie(b *testing.B) {
-	trie := NewTrie()
-	for n := 0; n < b.N; n++ {
-		trie.Del("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+	if Debug {
+		trie.Log()
 	}
 }
